Connected layer fixed, conv backprop fixed, templates, batchnorm experiments ongoing

model/activations.py
@@ -1,7 +1,8 @@
 import numpy as np
+from model.templates import Activation
 
 
-class ReLU:
+class ReLU(Activation):
 
     def __call__(self, x):
         return np.clip(x, 0, x.max())
@@ -9,23 +10,23 @@ class ReLU:
     def backward(self, x):
         calc = np.zeros_like(x)
         calc[x > 0] = 1
-        return calc
+        return calc, {}
 
 
-class Softmax:
+class Softmax(Activation):
 
     def __call__(self, x):
-        return np.exp(x) / np.sum(np.exp(x))
+        return np.exp(x) / np.sum(np.exp(x), axis=1).reshape((-1, 1))
 
     def backward(self, x):
         reshape = x.reshape(-1, 1)
-        return np.diagflat(reshape) - np.dot(reshape, reshape.T)
+        return np.diagflat(reshape) - np.dot(reshape, reshape.T), {}
 
 
-class Tanh:
+class Tanh(Activation):
 
     def __call__(self, x):
         return np.tanh(x)
 
     def backward(self, x):
-        return 1.0 - np.tanh(x)**2
+        return 1.0 - np.tanh(x)**2, {}

model/layers.py
@@ -1,21 +1,29 @@
 import numpy as np
 from model.utils import InvalidModeException
+from model.templates import Layer, Variable
 
 
-class Conv2D:
+class Conv2D(Layer):
 
     def __init__(self, size, step, filters):
+        """
+        Arguments:
+            size {float (int, int)}
+            step {int}
+            filters {int}
+        """
         self.size = size
         self.step = step
         self.filters = filters
         self.initialized = False
-        self.memorized_input = None
 
     def __call__(self, x):
 
         if not self.initialized:
             self._create_weights(x.shape)
 
+        weights = self.weights.value
+        biases = self.biases.value
         size_h, size_w = self.size
         step = self.step
         filters = self.filters
@@ -31,12 +39,12 @@ class Conv2D:
             w_idx = 0
             for w in range(new_width):
                 for c in range(filters):
-                    fragment = x[:, h_idx:h_idx+size_h, w_idx:w_idx+size_w, :]
-                    weights = self.weights[:, :, :, c]
-                    bias = self.biases[c]
-                    calc = fragment * weights
+                    inp_frag = x[:, h_idx:h_idx+size_h, w_idx:w_idx+size_w, :]
+                    weights_frag = weights[:, :, :, c]
+                    bias = biases[c]
+                    calc = inp_frag * weights_frag
                     calc = np.sum(calc, axis=(1, 2, 3))
-                    calc += bias.flatten()
+                    calc += bias
                     new_array[:, h, w, c] = calc
                 w_idx += step
             h_idx += step
@@ -49,12 +57,15 @@ class Conv2D:
         weights = np.random.standard_normal((size_h, size_w, inp_shape[-1], self.filters))
         weights *= 0.01  # now weights will be close but not equal to 0
         biases = np.zeros((self.filters,))
-        self.biases = biases.astype("float32")
-        self.weights = weights.astype("float32")
+        self.biases = Variable(biases)
+        self.weights = Variable(weights)
+        self.variables = [self.weights, self.biases]
         self.initialized = True
 
     def backward(self, grad):
 
+        weights = self.weights.value
+        biases = self.biases.value
         size_h, size_w = self.size
         step = self.step
         batch_size, height, width, channels = self.memorized_input.shape
@@ -62,8 +73,8 @@ class Conv2D:
         x = self.memorized_input
 
         input_grad = np.zeros_like(self.memorized_input)
-        weights_grad = np.zeros_like(self.weights)
-        bias_grad = np.zeros_like(self.biases)
+        weights_grad = np.zeros_like(weights)
+        bias_grad = np.zeros_like(biases)
 
         h_idx = 0
         for h in range(new_height):
@@ -73,23 +84,28 @@ class Conv2D:
                     grad_fragment = input_grad[:, h_idx:h_idx+size_h, w_idx:w_idx+size_w, :]
                     input_fragment = x[:, h_idx:h_idx+size_h, w_idx:w_idx+size_w, :]
 
-                    grad_fragment += self.weights[:, :, :, c] * grad[:, h, w, c]
-                    weights_grad[:, :, :, c] += np.sum(input_fragment * grad[:, h, w, c])
+                    stacked_weights = np.stack([weights[:, :, :, c]] * x.shape[0])
+                    grad_reshaped = grad[:, h, w, c].reshape((-1, 1, 1, 1))
+
+                    grad_fragment += np.sum(stacked_weights * grad_reshaped, axis=0)
+                    weights_grad[:, :, :, c] += np.sum(input_fragment * grad_reshaped, axis=0)
                     bias_grad[c] += np.sum(grad[:, h, w, c])
 
                 w_idx += step
             h_idx += step
-        return input_grad, weights_grad, bias_grad
+        return input_grad, {self.weights: weights_grad,
+                            self.biases: bias_grad}
 
 
-class Padding:
+class Padding(Layer):
 
     def __init__(self, size, mode='zero', **kwargs):
-        if mode not in ['zero', 'average', 'value']:
-            raise InvalidModeException(f"'{self.mode}' is not a valid mode")
+        if mode not in ['zero', 'mean', 'value']:
+            raise InvalidModeException(f"'{mode}' is not a valid mode")
 
         self.size = size
         self.mode = mode
+        self.variables = []
         if mode == 'value':
             if 'value' not in kwargs.keys():
                 raise ValueError("Mode set to 'value' but no value provided")
@@ -98,32 +114,36 @@ class Padding:
     def __call__(self, x):
         if self.mode == 'zero':
             value = 0
-        elif self.mode == 'average':
+        elif self.mode == 'mean':
             value = np.mean(x)
         elif self.mode == 'value':
             value = self.value
-        else:
-            raise InvalidModeException(f"'{self.mode}' is not a valid mode")
 
+        s = self.size
         return np.pad(array=x,
-                      pad_width=self.size,
+                      pad_width=[(0, 0), (s, s), (s, s), (0, 0)],
                       mode='constant',
                       constant_values=value)
 
     def backward(self, grad):
-        size_h, size_w = self.size
-        return grad[:, size_h:-size_h, size_w:-size_w, :]
+        s = self.size
+        return grad[:, s:-s, s:-s, :], {}
 
 
-class Pooling:
+class Pooling(Layer):
 
     def __init__(self, size, step, mode='max'):
+        """
+        Arguments:
+            size {tuple (int, int)}
+            step {int}
+            mode {str} -- aviable modes: ["max", "mean"] (default: {'max'})
+        """
         self.size = size
         self.step = step
         if mode.lower() not in ['max', 'mean']:
             raise InvalidModeException(f"'{mode}' is not a valid mode")
         self.mode = mode.lower()
-        self.memorized_input = None
 
     def __call__(self, x):
 
@@ -182,26 +202,117 @@ class Pooling:
                         grad_fragment += np.full((batch_size, size_h, size_w), mean_val)
                 w_idx += step
             h_idx += step
-        return gradient
+        return gradient, {}
 
 
-class Flat:
+class FullyConnected(Layer):
 
     def __init__(self, units):
+        """
+        Arguments:
+            units {int}
+        """
         self.units = units
         self.weights = None
-        self.biases = np.zeros(units).astype("float32")
+        self.biases = None
         self.initialized = False
-        self.memorized_input = None
 
     def __call__(self, x):
         if not self.initialized:
             self._create_weights(x.shape)
+
+        weights = self.weights.value
+        biases = self.biases.value
+
         self.memorized_input = x
-        return np.dot(x, self.weights) + self.biases
+        return np.dot(x, weights) + biases
 
     def _create_weights(self, inp_shape):
         weights = np.random.standard_normal((inp_shape[-1], self.units))
         weights *= 0.01  # now weights will be close but not equal to 0
-        self.weights = weights.astype("float32")
+        biases = np.zeros((self.units,))
+        self.weights = Variable(weights)
+        self.biases = Variable(biases)
+        self.variables = [self.weights, self.biases]
         self.initialized = True
+
+    def backward(self, grad):
+
+        weights = self.weights.value
+
+        input_grad = np.dot(grad, weights.T)
+        weights_grad = np.dot(grad.T, self.memorized_input).T
+        bias_grad = np.sum(grad, axis=0)
+
+        return input_grad, {self.weights: weights_grad,
+                            self.biases: bias_grad}
+
+
+class Flatten(Layer):
+
+    def __init__(self):
+        self.memorized_input = None
+        self.variables = []
+
+    def __call__(self, x):
+        self.memorized_input = x
+        return x.reshape((x.shape[0], -1))
+
+    def backward(self, grad):
+        shape = self.memorized_input.shape
+        return grad.reshape(shape), {}
+
+
+class BatchNorm(Layer):
+
+    def __init__(self, epsilon=1e-8):
+        self.epsilon = epsilon
+
+    def __call__(self, x):
+
+        batch_size, height, width, channels = x.shape
+        epsilon = self.epsilon
+        gamma = self.gamma
+        beta = self.beta
+
+        mean = np.mean(x, axis=0)
+        var = np.var(x, axis=0)
+
+        norm = (x - mean) / np.sqrt(var + epsilon)
+        calc = gamma * norm + beta
+
+        self.memorized_input = x
+        return calc
+
+    def _create_weights(self, inp_shape):
+        gamma = 1.0
+        beta = 0.0
+        self.gamma = gamma
+        self.beta = beta
+        self.variables = [self.gamma, self.beta]
+
+    def backward(self, grad):
+        x = self.memorized_input
+        mean = np.mean(x, axis=0)
+        var = np.var(x, axis=0)
+        gamma = self.gamma
+
+        epsilon = self.epsilon
+
+        x_min_mean = x - mean
+        std = np.sqrt(var + epsilon)
+
+        inp_grad = grad * gamma
+        var_grad = np.sum(x_min_mean * inp_grad, axis=0)
+        var_grad *= (-0.5 * ((1.0 / std) ** 3))
+        mean_grad = np.sum(inp_grad * (1.0 / std), axis=0)
+        mean_grad += var_grad * np.mean(-2.0 * x_min_mean, axis=0)
+
+        inp_grad *= (1.0 / std)
+        inp_grad += (var_grad * 2 * (x_min_mean / x.shape[0])) + (mean_grad / x.shape[0])
+
+        gamma_grad = np.sum(grad * (x_min_mean / std), axis=0)
+        beta_grad = np.sum(grad, axis=0)
+
+        return inp_grad, {self.gamma: gamma_grad,
+                          self.beta: beta_grad}