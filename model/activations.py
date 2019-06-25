Activations backprop fix

model/activations.py
@@ -5,20 +5,24 @@ from model.templates import Activation
 class ReLU(Activation):
 
     def __call__(self, x):
+        self.memorized_input = x.copy()
         return np.clip(x, 0, x.max())
 
-    def backward(self, x):
-        calc = np.zeros_like(x)
-        calc[x > 0] = 1
+    def backward(self, grad):
+        x = self.memorized_input
+        calc = np.array(grad, copy=True)
+        calc[x <= 0] = 0
         return calc, {}
 
 
 class Softmax(Activation):
 
     def __call__(self, x):
-        return np.exp(x) / np.sum(np.exp(x), axis=1).reshape((-1, 1))
+        self.memorized_input = x.copy()
+        return np.exp(x) / (np.sum(np.exp(x), axis=1).reshape((-1, 1)))
 
-    def backward(self, x):
+    def backward(self, grad):
+        x = self.memorized_input
         reshape = x.reshape(-1, 1)
         return np.diagflat(reshape) - np.dot(reshape, reshape.T), {}
 
@@ -26,7 +30,9 @@ class Softmax(Activation):
 class Tanh(Activation):
 
     def __call__(self, x):
+        self.memorized_input = x.copy()
         return np.tanh(x)
 
-    def backward(self, x):
-        return 1.0 - np.tanh(x)**2, {}
+    def backward(self, grad):
+        x = self.memorized_input
+        return (1.0 - (np.tanh(x) ** 2)) * grad, {}